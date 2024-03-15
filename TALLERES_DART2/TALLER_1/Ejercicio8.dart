cdb66951cf4bd95857f010107eab8ab90aa3fe8b
Jordy-int <jordy.sneider13@gmail.com>
2024-03-15 16:15:42 -0500

    Talleres

--- TALLERES_DART2/TALLER_1/Ejercicio8.dart
+++ TALLERES_DART2/TALLER_1/Ejercicio8.dart
@@ -1,1 +1,24 @@
+import 'dart:io';
 
+void main() {
+  print("Ingrese una palabra:");
+  String palabra = stdin.readLineSync()!.toLowerCase();
+  Map<String, int> contarVocales = {
+    'a': 0,
+    'e': 0,
+    'i': 0,
+    'o': 0,
+    'u': 0,
+  };
+
+  for (int i = 0; i < palabra.length; i++) {
+    String letra = palabra[i];
+    if (contarVocales.containsKey(letra)) {
+      contarVocales[letra] = contarVocales[letra]! + 1;
+    }
+  }
+
+  contarVocales.forEach((vocal, conteo) {
+    print('La vocal $vocal aparece $conteo veces.');
+  });
+}
